d742d704cdd73218c1d781476527113f12ecb48d
K Mounisha sri sai saranya <153306027+mounishaa-k@users.noreply.github.com>
2024-08-13 13:00:39 +0530

    Update MusicPlayerMain.java

--- ThreeB/MusicPlayerMain.java
+++ ThreeB/MusicPlayerMain.java
@@ -2,7 +2,7 @@ package DEMO;
 
 public class MusicPlayerMain {
     public static void main(String[] args) {
-        // Using the Play class with MusicAdapter (which now uses the Bridge Pattern)
+       
         Play player = new Play();
 
         player.play("online", "beyond the horizon.mp3");
@@ -10,7 +10,6 @@ public class MusicPlayerMain {
         player.play("radio", "far far away.mp3");
         player.play("localfile", "mind me.mp3");
 
-        // Demonstrating the use of decorators with the bridge
         MusicPlayerBridge decoratedLocalPlayer = new MusicPlayerBridge(new LocalFileSource());
         MusicPlayer decoratedWithEqualizer = new EqualizerDecorator(new BasicMusicPlayer(decoratedLocalPlayer));
         VolumeControlDecorator decoratedWithVolumeControl = new VolumeControlDecorator(decoratedWithEqualizer);
