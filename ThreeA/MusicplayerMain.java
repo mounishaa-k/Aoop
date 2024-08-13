02ed594225d3aaf294d09920bdb1f30a1c3233a7
K Mounisha sri sai saranya <153306027+mounishaa-k@users.noreply.github.com>
2024-08-13 12:58:19 +0530

    Update MusicplayerMain.java

--- ThreeA/MusicplayerMain.java
+++ ThreeA/MusicplayerMain.java
@@ -6,15 +6,13 @@ public class MusicplayerMain {
         MusicPlayer onlinePlayer = new Online();
         MusicPlayer radioPlayer = new Radio();
 
-        // Adding Equalizer and Volume Control to Local Player
         MusicPlayer decoratedLocalPlayer = new VolumeControlDecorator(new EqualizerDecorator(localPlayer));
         decoratedLocalPlayer.playLocalFile("alone.mp3");
 
-        // Adding Equalizer to Online Player
+  
         MusicPlayer decoratedOnlinePlayer = new EqualizerDecorator(onlinePlayer);
         decoratedOnlinePlayer.playOnline("beyond the horizon.mp3");
 
-        // Adding Volume Control to Radio Player
         MusicPlayer decoratedRadioPlayer = new VolumeControlDecorator(radioPlayer);
         decoratedRadioPlayer.playRadio("far far away.mp3");
     }
